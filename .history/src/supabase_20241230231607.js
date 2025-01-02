e8fe3885c861cfcbd853a13eece631372b8291df
dev-athar <devathar11@gmail.com>
2025-01-02 19:46:42 +0600

    Remove .history folder from the repository

--- .history/src/supabase_20241230231607.js
+++
@@ -1,8 +0,0 @@
-// src/supabase.js
-import { createClient } from "@supabase/supabase-js";
-
-// Replace with your Supabase project details
-const SUPABASE_URL = "https://sifneaujnvssmmholqep.supabase.co";
-const SUPABASE_ANON_KEY = "<your-anon-key>";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
--- .history/src/supabase_20241230231620.js
+++
@@ -1,9 +0,0 @@
-// src/supabase.js
-import { createClient } from "@supabase/supabase-js";
-
-// Replace with your Supabase project details
-const SUPABASE_URL = "https://sifneaujnvssmmholqep.supabase.co";
-const SUPABASE_ANON_KEY =
-  "<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpZm5lYXVqbnZzc21taG9scWVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU1Nzg3OTYsImV4cCI6MjA1MTE1NDc5Nn0.zF7HMOg0-BrktsjLbYnVq5kqcp5-WHRwPWzEX9ggRew>";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
